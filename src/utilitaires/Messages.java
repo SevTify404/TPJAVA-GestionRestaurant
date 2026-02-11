c6af331de1627b6cfe142601374150acd10f483b
SevTify[404] <gametech228@gmail.com>
2026-02-11 08:41:28 +0000

    Ajoutde la classe Messages pour centraliser les messsages et rendre réutilisable

---
+++ src/utilitaires/Messages.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilitaires;
+
+/**
+ *
+ * @author sevtify
+ */
+public final class Messages {
+    
+    // Ici on va stocker tous les messages qui seront utilisés dans l'application
+    // et on va rendre dynamique avec String.format
+    
+    public final static String LOGIN_SUCCESS = "Connexion réussie !";
+
+    public final static String LOGIN_FAILED = "Login ou mot de passe incorrect.";
+
+    // Ainsi de suite
+    
+    
+}
