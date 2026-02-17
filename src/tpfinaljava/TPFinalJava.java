05d3045aa197edb7cb966bbe36977330ec58f31a
loyale-005 <royaleloyale25@email.com>
2026-02-17 18:39:00 +0000

    Resolution des conflits

--- src/tpfinaljava/TPFinalJava.java
+++ src/tpfinaljava/TPFinalJava.java
@@ -26,35 +26,12 @@ public class TPFinalJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< Upstream, based on origin/main
         CrudResult<List<Audit>> ee = AuditDAO.getInstance().recupererTout();
         System.out.println(ee);
         if (ee.estUnSucces()) {
             System.out.println(ee.getDonnes());
         }
-=======
-        System.out.println("===== TEST INSERT COMMANDE =====");
 
-    Commande com = new Commande();
-
-    // ❗ NE PAS METTRE L’ID SI AUTO_INCREMENT
-
-    com.setDateCommande(LocalDateTime.now());
-
-    // 🔴 À ADAPTER SELON TA TABLE
-    com.setIdCommande(1);
-    com.setDateCommande(LocalDateTime.now());
-
-    CrudResult<Boolean> res =
-            CommandeDAO.getInstance().enregistrer(com);
-
-    if (res.estUnSucces()) {
-        System.out.println("Insertion réussie");
-        System.out.println("ID généré : " + com.getIdCommande());
-    } else {
-        System.out.println("Erreur : " + res.getErreur());
-    }
->>>>>>> 07a0b97 LigneCommandeDAO
     }
     
 }
