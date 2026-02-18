5d25f80e6ae244ae80f70b94f54c576e4a8ed158
SevTify[404] <gametech228@gmail.com>
2026-02-18 15:07:37 +0000

    fix: fix de Main

--- src/tpfinaljava/Main.java
+++ src/tpfinaljava/Main.java
@@ -3,19 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tpfinaljava;
-
-<<<<<<< HEAD:src/tpfinaljava/Main.java
 import formulaires.App;
 import utilitaires.VariablesEnvirennement;
-=======
-import dao.AuditDAO;
-
-
-
-import dao.CrudResult;
-import entity.Audit;
-import java.util.List;
->>>>>>> main:src/tpfinaljava/TPFinalJava.java
 
 
 /**
@@ -28,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD:src/tpfinaljava/Main.java
+
         
         // Je verifie si le fiechier .env existe
         VariablesEnvirennement.checkVariablesEnvironnement();
@@ -36,17 +25,6 @@ public class Main {
         App application = App.getInstance();
         
         application.lancerApplication();
+
     }
-=======
-        CrudResult<List<Audit>> ee = AuditDAO.getInstance().recupererTout();
-        System.out.println(ee);
-        if (ee.estUnSucces()) {
-            System.out.println(ee.getDonnes());
-        }
-       
-    }
-    
-    
-    
->>>>>>> main:src/tpfinaljava/TPFinalJava.java
 }
