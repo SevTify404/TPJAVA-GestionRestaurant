6cd40583c76f84a6666a08af481c6f358f7b2c13
SevTify[404] <gametech228@gmail.com>
2026-02-18 21:34:27 +0000

    fix: Transformation des valeurs absolues en valeurs relatives au niveau de l'écran principal et des boutons du JFRAME du Menu Principal

--- src/tpfinaljava/Main.java
+++ src/tpfinaljava/Main.java
@@ -17,13 +17,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
         
+
         // Je verifie si le fiechier .env existe
         VariablesEnvirennement.checkVariablesEnvironnement();
         
         App application = App.getInstance();
-        
+      
         application.lancerApplication();
 
     }
